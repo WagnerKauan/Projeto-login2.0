b9e6639dda16e9b5f7130935c1ce9a3609a844b3
Wagner Kauan <kauanw711@gmail.com>
2024-01-30 23:23:12 -0300

    .

---
+++ teste.js
@@ -0,0 +1,15 @@
+function testando1(){
+    var login = document.querySelector('.contain')
+
+    login.style.left = '0%'
+    login.style.transition = '0.5s'
+    
+}
+
+function testando2(){
+    var login = document.querySelector('.contain')
+
+    login.style.left = '50%'
+    login.style.transition = '0.5s'
+    
+}
