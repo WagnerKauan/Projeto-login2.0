9324b8b5a93548c240f7a5eb9ba07db7a571713d
Wagner Kauan <kauanw711@gmail.com>
2024-02-03 13:43:35 -0300

    Ajustando

--- funcionalidades.js
+++ funcionalidades.js
@@ -2,6 +2,7 @@ function registrar(){
     var hove = document.querySelector('.hove')
     var texto1 = document.querySelector('.text-1')
     var texto2 = document.querySelector('.text-2')
+    var check = document.querySelector('#check')
 
     hove.style.left = '0%'
     hove.style.transition = '0.5s'
@@ -10,19 +11,21 @@ function registrar(){
     texto1.style.transform = 'scaleX(-1)'
     texto2.style.transform = 'scaleX(-1)'
 
-   
+    check.checked = true
 }
 
 function login(){
     var hove = document.querySelector('.hove')
     var texto1 = document.querySelector('.text-1')
     var texto2 = document.querySelector('.text-2')
+    var check = document.querySelector('#check')
 
-    hove.style.left = '50%'
+    hove.style.left = '50%' 
     hove.style.transition = '0.5s'
     hove.style.transform = 'scaleX(1)'
 
     texto1.style.transform = 'scaleX(1)'
     texto2.style.transform = 'scaleX(1)'
 
+    check.checked = false
 }
